ca990889cebcdcb41c2ad73fc79a0271de0c2d4c
Duc Minh <https://github.com/DucMinh2412/D-n-1---PT14353>
2020-10-15 14:43:37 +0700

    Prefect

--- app/RetrofitPro/src/main/java/com/example/retrofitpro/GenericRetrofit.java
+++ app/RetrofitPro/src/main/java/com/example/retrofitpro/GenericRetrofit.java
@@ -1,5 +1,8 @@
 package com.example.retrofitpro;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.google.gson.Gson;
@@ -15,24 +18,49 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class GenericRetrofit {
+public class GenericRetrofit<T>{
+    String link;
+    String request;
+    Activity activity;
+    List<T> list;
 
-    public static <T extends Object> void callRetrofitArray(String link, String request, List<T> list) {
+    public GenericRetrofit(String link, String request, Activity activity, List<T> list) {
+        this.link = link;
+        this.request = request;
+        this.activity = activity;
+        this.list = list;
+    }
+
+    public void GetDataJsonArray() {
         Retrofit restRetrofit = RestRetrofit.getInstance(link);
         RetrofitService retrofitService = restRetrofit.create(RetrofitService.class);
         retrofitService.getArray(request).enqueue(new Callback<JsonArray>() {
             @Override
             public void onResponse(Call<JsonArray> call, Response<JsonArray> response) {
-                String jsonString = response.body().toString();
-                Type listType = new TypeToken<List<T>>() {}.getType();
-                list.addAll(new Gson().fromJson(jsonString, listType));
-                Log.e("Success","Success");
+                if(response.isSuccessful()) {
+                    String jsonString = response.body().toString();
+                    SaveData(jsonString);
+                }
             }
 
             @Override
             public void onFailure(Call<JsonArray> call, Throwable t) {
-                 Log.e("Error","Error " + t);
+                Log.e("Error","Error " + t);
             }
         });
+    }
+
+    private void SaveData(String JsonString){
+        SharedPreferences sharedPreferences = activity.getSharedPreferences("MY_SHARE.f", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("JsonString", JsonString);
+        editor.commit();
+    }
+
+    public void GetData(){
+        SharedPreferences prefs = activity.getSharedPreferences("MY_SHARE.f", Context.MODE_PRIVATE);
+        String JsonString = prefs.getString("JsonString",null);
+        Type listType = new TypeToken<List<T>>() {}.getType();
+        list.addAll(new Gson().fromJson(JsonString,listType));
     }
 }
